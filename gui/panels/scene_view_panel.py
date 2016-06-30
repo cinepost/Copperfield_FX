31391157d4ba1e9f6b7e23c4b266a03860b650fc
Max <kirienko.max@gmail.com>
2016-06-30 12:01:23 +0200

    SceneView background, grid and origin

--- gui/panels/scene_view_panel.py
+++ gui/panels/scene_view_panel.py
@@ -24,7 +24,7 @@ class SceneViewPanel(BasePanel):
 
     @classmethod
     def panelTypeName(cls):
-        return "Network View"
+        return "Scene View"
 
     @classmethod
     def hasNetworkControls(cls):
@@ -36,40 +36,154 @@ class SceneViewWidget(QtOpenGL.QGLWidget):
         format = QtOpenGL.QGLFormat.defaultFormat()
         format.setSampleBuffers(True)
         format.setSamples(16)
-        QtOpenGL.QGLWidget.__init__(self, parent)
-        self.width = 640
-        self.height = 480
-        self.setMinimumSize(640, 480)
+        QtOpenGL.QGLWidget.__init__(self, format, parent)
+        self.width = 1902
+        self.height = 1200
+        self.setMinimumSize(640, 360)
 
-    def paintGL(self):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()
+    def drawBackground(self):
+        glDisable(GL_DEPTH_TEST)
+        glBegin(GL_QUADS)
+        glColor3f(0.39, 0.50, 0.55)
+        glVertex2f(0.0, 0.0)
+        glVertex2f(self.width, 0.0)
 
-        glTranslatef(-2.5, 0.5, -6.0)
-        glColor3f( 1.0, 1.5, 0.0 );
-        glPolygonMode(GL_FRONT, GL_FILL);
+        glColor3f(0.75, 0.78, 0.78)
+        glVertex2f(self.width, self.height)
+        glVertex2f(0.0, self.height)
+        glEnd()
+        glEnable(GL_DEPTH_TEST)
+
+    def drawSceneGrid(self):
+        grid_step = 0.1
+        grid_size = 4.0
+        grid_half_size = grid_size / 2.0
+
+        # Minor lines
+        lines_quantity = int(grid_size / grid_step) + 1
+        glColor3f( 0.35, 0.35, 0.35 )
+        glLineWidth(0.5)
+
+        # Draw lines
+        glBegin(GL_LINES)
+        x_coord = - grid_half_size
+        for x in range(lines_quantity):
+            glVertex3f(x_coord, 0.0, -grid_half_size)
+            glVertex3f(x_coord, 0.0, grid_half_size)
+            x_coord += grid_step
+
+        z_coord = - grid_half_size
+        for z in range(lines_quantity):
+            glVertex3f(-grid_half_size, 0.0, z_coord)
+            glVertex3f(grid_half_size, 0.0, z_coord)
+            z_coord += grid_step
+
+        glEnd()
+
+        # Major lines
+        grid_step *= 10
+        lines_quantity = int(grid_size / grid_step) + 1
+        glLineWidth(1.0)
+
+        # Draw lines
+        glBegin(GL_LINES)
+        x_coord = - grid_half_size
+        for x in range(lines_quantity):
+            glVertex3f(x_coord, 0.0, -grid_half_size)
+            glVertex3f(x_coord, 0.0, grid_half_size)
+            x_coord += grid_step
+
+        z_coord = - grid_half_size
+        for z in range(lines_quantity):
+            glVertex3f(-grid_half_size, 0.0, z_coord)
+            glVertex3f(grid_half_size, 0.0, z_coord)
+            z_coord += grid_step
+
+        glEnd()
+
+        # Draw Origin
+        glDisable(GL_DEPTH_TEST)
+        glLineWidth(1.0)
+        glBegin(GL_LINES)
+        # X axis
+        glColor3f( 0.95, 0.05, 0.05 )
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.8, 0.0, 0.0)
+        # Y axis
+        glColor3f( 0.05, 0.95, 0.05 )
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 0.8, 0.0)
+        # z axis
+        glColor3f( 0.05, 0.05, 0.95 )
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 0.0, 0.8)
+        glEnd()
+
+        glEnable(GL_DEPTH_TEST)
+
+    def drawSceneObjects(self):
+        glTranslatef(0.0, 0.0, 0.0)
+        glColor3f( 1.0, 0.6, 0.2 )
+        glPolygonMode(GL_FRONT, GL_FILL)
 
         glBegin(GL_TRIANGLES)
-        glVertex3f(2.0,-1.2,0.0)
-        glVertex3f(2.6,0.0,0.0)
-        glVertex3f(2.9,-1.2,0.0)
+        glVertex3f(0.0, 1.0, 0.0)
+        glVertex3f(1.0, -1.0, 0.0)
+        glVertex3f(-1.0,-1.0, 0.0)
         glEnd()
+
+    def paintGL(self):
+        glClearColor(0.5, 0.5, 0.5, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Drab back plane elements
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, self.width, self.height, 0.0, -100.0, 100.0)
+
+        # Background
+        self.drawBackground()
+
+        # Draw scene
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0,float(self.width)/float(self.height),0.1, 1000.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(5, 5, 5, 0, 0, 0, 0, 1, 0)
+
+        # Grid
+        self.drawSceneGrid()
+
+        # Scene objects
+        self.drawSceneObjects()
 
         glFlush()
 
     def resizeGL(self, width, height):
         self.width = width
         self.height = height
+        
+        if self.isValid():
+            glViewport(0, 0, width, height)
+            glMatrixMode(GL_PROJECTION)
+            glLoadIdentity()
+            gluPerspective(45.0,float(width)/float(height),0.1, 1000.0)
+            glMatrixMode(GL_MODELVIEW)
 
     def initializeGL(self):
         glClearDepth(1.0)              
         glDepthFunc(GL_LESS)
         glEnable(GL_DEPTH_TEST)
+        #glEnable(GL_MULTISAMPLE)
+        #glEnable(GL_LINE_SMOOTH)
         glShadeModel(GL_SMOOTH)
 
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()                    
-        gluPerspective(45.0,1.33,0.1, 100.0) 
-        glMatrixMode(GL_MODELVIEW)
+        #glMatrixMode(GL_PROJECTION)
+        ##lLoadIdentity()                    
+        #gluPerspective(45.0,1.33,0.1, 100.0) 
+        #glMatrixMode(GL_MODELVIEW)
 
 

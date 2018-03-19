b6b076cc33b70e95204d51d14966005ee2b56198
Max <kirienko.max@gmail.com>
2018-03-19 08:28:46 +0100

    xz

--- shout/parsers/rib/parser_rib.py
+++ shout/parsers/rib/parser_rib.py
@@ -1,5 +1,8 @@
+import logging
 from pyparsing import *
 from ..base import ParserBase
+
+logger = logging.getLogger(__name__)
 
 rib_grammar = None
 
@@ -18,7 +21,6 @@ class ParserRIB(ParserBase):
 		global rib_grammar
 
 		if rib_grammar is None:
-
 			# commons
 			point = Literal('.')
 			e = CaselessLiteral('E')
@@ -32,53 +34,21 @@ class ParserRIB(ParserBase):
 			# IFD types
 
 			# IFD commands
-			setenv = Keyword('setnev')
-			ray_version = Keyword('ray_version') + Word(alphas + nums + ".")
-			
-			ray_detail_name = Word(printables)
-			ray_detail_filename = string | Word(printables)
-			ray_detail_1 = Keyword("ray_detail") + Group(Optional(Keyword("-T")) + ray_detail_name + ray_detail_filename)
-			ray_detail_1.setParseAction(self.do_ray_detail_1)
+			world_begin = Keyword('WorldBegin')
+			world_begin.setParseAction(self.do_world_begin)
 
-			ray_detail_sourcename = string | Word(printables)
-			ray_detail_2 = Keyword("ray_detail") + Group(Optional(Keyword("-v") + floatnum3) | Optional(Keyword("-V") + floatnum3 + floatnum3) + ray_detail_name + ray_detail_sourcename)
+			world_end = Keyword('WorldEnd')
+			world_end.setParseAction(self.do_world_end)
 
-			ray_declare_style = oneOf("object global light geometry plane")
-			ray_declare_type = oneOf("float bool int vector2 vector3 vector4 matrix3 matrix4 string")
-			ray_declare_array_size = Suppress(Keyword("-v")) + integer
-			ray_declare_name = Word(printables)
-			ray_declare_value = string | number | Word(alphanums)
-			ray_declare = Keyword('ray_declare') + Group(Optional(ray_declare_array_size) + ray_declare_style + ray_declare_type + ray_declare_name + ray_declare_value)
-			
-			ray_start_object_type = oneOf('material geo light fog object instance plane segment')
-			ray_start = Keyword('ray_start') + ray_start_object_type
-			
-			ray_end = Keyword('ray_end')
-			ray_time = Keyword('ray_time') + floatnum
-
-			ray_image_name = string
-			ray_image = Group(Keyword('ray_image') + ray_image_name)
-			
-			ray_procedural_bbox = Group(Suppress(Keyword("-m")) + floatnum3 + Suppress(Keyword("-M")) + floatnum3)
-			ray_procedural = Keyword('ray_procedural') + Optional(ray_procedural_bbox)
-			
-			ray_property_style = oneOf('renderer image camera object light plane')
-			ray_property_token = Word(alphanums)
-			ray_property_value = Group(OneOrMore(integer | floatnum | Word(alphanums + "_") | string))
-			ray_property = Keyword('ray_property') + Group(ray_property_style + ray_property_token + ray_property_value)
-			
-			ray_transform = Keyword('ray_transform') + Group(OneOrMore(floatnum))
-			ray_geometry = Keyword('ray_geometry')
-			ray_raytrace = Keyword('ray_raytrace')
-			ray_deviceoption = Keyword('ray_deviceoption')
-			ray_reset = Keyword('ray_reset') + Group(OneOrMore(oneOf("-l -o -f")))
-			ray_quit = Keyword('ray_raytrace').setParseAction(self.finish)
-
-			set_env = Keyword('setenv') + Group(Word(alphanums) + Suppress(Literal('=')) + string)
-
-			otprefer = Keyword('otprefer') + string + string
-
-			rib_grammar = (world_begin | world_end | Word(printables))
+			rib_grammar = Optional(world_begin | world_end)
 			rib_grammar.ignore('#' + restOfLine) # ignore comments
 
 		return rib_grammar
+
+	def do_world_begin(self, tokens):
+		logger.debug(tokens)
+
+	def do_world_end(self, tokens):
+		logger.debug(tokens)
+
+
